0e6ff5a53b54e73964bd14e80189a46e618d3d96
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2024-11-27 11:55:22 -0800

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, ...itemsToRemove) {
+  result = [];
+  myArray.forEach((element) => {
+    if (!itemsToRemove.includes(element)) {
+      result.push(element);
+    }
+  })
+  return result;
 };
 
 // Do not edit below this line
