1c556bcc07f02f0330f8002210e4b8a3906bdb72
Dane Miyata <48339284+dymiyata@users.noreply.github.com>
2024-11-27 11:36:18 -0800

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, frequency) {
+  if (frequency < 0) {
+    return 'ERROR'
+  }
+  result = '';
+  for (let i = 0; i < frequency; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
